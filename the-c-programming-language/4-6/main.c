08b2c7aff6c7e237ccacbb4ab17c955139f105a5
elaijuh <hjiale@gmail.com>
2017-12-09 02:28:11 +0800

    tcpl 4-6

---
+++ the-c-programming-language/4-6/main.c
@@ -0,0 +1,76 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+#include <ctype.h>
+#include "calc.h"
+
+#define MAXOP 100
+
+int main(void) {
+    int type;
+    double op2;
+    char s[MAXOP];
+
+    int v;
+    double vars[26];
+    double last;
+
+    while ((type = getop(s)) != EOF) {
+        switch (type) {
+            case NUMBER:
+                push(atof(s));
+                break;
+            case OPERATOR:
+                if (strcmp(s, "sin") == 0)
+                    push(sin(pop()));
+                else if (strcmp(s, "exp") == 0)
+                    push(exp(pop()));
+                else if (strcmp(s, "pow") == 0) {
+                    op2 = pop();
+                    push(pow(pop(), op2));
+                }
+                else
+                    printf("error: unknow command %s\n", s);
+                break;
+            case VARIABLE:
+                v = tolower(s[0]);
+                last = vars[v - 'a'] = get_top();
+                printf("%c = %.8g\n", v, vars[v - 'a']);
+                break;
+            case '+':
+                push(pop() + pop());
+                break;
+            case '*':
+                push(pop() * pop());
+                break;
+            case '-':
+                op2 = pop();
+                push(pop() - op2);
+                break;
+            case '/':
+                op2 = pop();
+                if (op2 != 0.0) 
+                    push(pop() / op2);
+                else
+                    printf("error: zero divisor\n");
+                break;
+            case '%':
+                op2 = pop();
+                if (op2 != 0.0) 
+                    push(fmod(pop(), op2));
+                else
+                    printf("error: zero divisor\n");
+                break;
+            case '\n':
+                print_top();
+                clean();
+                break;
+            default:
+                printf("error: unknown command %s\n", s);
+                break;
+        }
+    }
+
+    return 0;
+}
